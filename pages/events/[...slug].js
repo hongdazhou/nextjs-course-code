e7c1d99941ef5dcf98ebc4d68a5f22c8878e2dc7
hongdazhou <zhouhongda@gmail.com>
2021-04-02 09:55:53 -0400

    commit1

---
+++ pages/events/[...slug].js
@@ -0,0 +1,5 @@
+function FilteredEventsPage() {
+  return <div></div>;
+}
+
+export default FilteredEventsPage;
---
+++ pages/events/[eventId].js
@@ -0,0 +1,5 @@
+function EventDetailPage() {
+  return <div></div>;
+}
+
+export default EventDetailPage;
