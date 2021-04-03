804757c30007fc08f24bbca138cf56c14001af3c
hongdazhou <zhouhongda@gmail.com>
2021-04-02 21:53:26 -0400

    finished-routing

--- pages/events/index.js
+++ pages/events/index.js
@@ -2,11 +2,19 @@ import { Fragment } from "react";
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
 import EventSearch from "../../components/events/event-search";
+import { useRouter } from "next/router";
+
 function AllEventsPage() {
   const events = getAllEvents();
+  const router = useRouter();
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
+
   return (
     <Fragment>
-      <EventSearch />
+      <EventSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </Fragment>
   );
